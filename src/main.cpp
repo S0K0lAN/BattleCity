7e39336a9125a16aad30bf81c5f1453decb116f8
Andrew <sokolov3indastris@gmail.com>
2022-08-11 14:46:40 +0300

    Draw Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -31,11 +31,11 @@ const char* vertex_shader =
 "}";
 
 const char* fragment_shader =
-"#version 460"
+"#version 460\n"
 "in vec3 color;"
 "out vec4 frag_color;"
 "void main() {"
-"   frag_color = vec4(color, 1);"
+"   frag_color = vec4(color, 1.0);"
 "}";
 
 
@@ -114,7 +114,7 @@ int main(void)
     GLuint shader_program = glCreateProgram();
     glAttachShader(shader_program, vs);
     glAttachShader(shader_program, fs);
-    glLineWidth(shader_program);
+    glLinkProgram(shader_program);
 
     glDeleteShader(vs);
     glDeleteShader(fs);
@@ -127,8 +127,19 @@ int main(void)
     GLuint colors_vbo = 0;
     glGenBuffers(1, &colors_vbo);
     glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
 
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
 
     /* Loop until the user closes the window */
@@ -136,6 +147,10 @@ int main(void)
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(pWindow);
